750fe935c42a252ae4e57c67c46bd1b6f8803877
Eik Emil Christensen <me@eikc.dk>
2016-07-28 16:41:46 +0200

    removed tests for green build

--- src/Test/Class1.cs
+++ src/Test/Class1.cs
@@ -10,11 +10,11 @@ namespace MyFirstDotNetCoreTests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+        //[Fact]
+        //public void FailingTest()
+        //{
+        //    Assert.Equal(5, Add(2, 2));
+        //}
 
         int Add(int x, int y)
         {
@@ -24,7 +24,7 @@ namespace MyFirstDotNetCoreTests
         [Theory]
         [InlineData(3)]
         [InlineData(5)]
-        [InlineData(6)]
+        //[InlineData(6)]
         public void MyFirstTheory(int value)
         {
             Assert.True(IsOdd(value));
