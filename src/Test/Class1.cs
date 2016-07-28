d1ac26dbfb7f6d5dbac15d9e01f9122160e8c566
Eik Emil Christensen <me@eikc.dk>
2016-07-28 16:50:54 +0200

    tralala making a failing build

--- src/Test/Class1.cs
+++ src/Test/Class1.cs
@@ -10,11 +10,11 @@ namespace MyFirstDotNetCoreTests
             Assert.Equal(4, Add(2, 2));
         }
 
-        //[Fact]
-        //public void FailingTest()
-        //{
-        //    Assert.Equal(5, Add(2, 2));
-        //}
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
 
         int Add(int x, int y)
         {
