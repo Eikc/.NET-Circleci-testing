392d382d565d8dd10c407aef6f01af638baab627
Eik Emil Christensen <me@eikc.dk>
2016-07-29 10:18:57 +0200

    tralala

--- src/Test/Class2.cs
+++ src/Test/Class2.cs
@@ -2,19 +2,13 @@
 
 namespace Test
 {
-    public class Class1
+    public class Class2
     {
         [Fact]
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
         }
-
-        //[Fact]
-        //public void FailingTest()
-        //{
-        //    Assert.Equal(5, Add(2, 2));
-        //}
 
         int Add(int x, int y)
         {
@@ -24,7 +18,7 @@ namespace Test
         [Theory]
         [InlineData(3)]
         [InlineData(5)]
-        //[InlineData(6)]
+        [InlineData(7)]
         public void MyFirstTheory(int value)
         {
             Assert.True(IsOdd(value));
